5e646263b717a7d64fa841d386cac2bc8c6d5518
Henning Redestig <henning.red@gmail.com>
2017-03-28 16:15:32 +0200

    feat: more than one model per species
    Support having more than one model per species by adding the optional
    `model_id` parameter and serving model-options per sample via by
    re-mapping the species identifiers from iloop to ncbitaxon/uniprot.

--- tests/test_end_to_end.py
+++ tests/test_end_to_end.py
@@ -9,13 +9,16 @@ class TestUM:
         self.iloop = iloop_client(Default.ILOOP_API, Default.ILOOP_TOKEN)
         self.experiment = self.iloop.Experiment.instances(where={'type': 'fermentation'})[0]
         self.sample = self.experiment.read_samples()[0]
+        self.model = {'ECO': 'iJO1366', 'SCE': 'iMM904'}[self.sample.strain.organism.short_code]
 
     def test_request(self):
         URLS = [
+            '/samples/{}/maximum-yield?model-id={}'.format(self.sample.id, self.model),
             '/samples/{}/maximum-yield'.format(self.sample.id),
             '/samples/{}/model?phase-id=1&with-fluxes=1&method=room'.format(self.sample.id),
             '/samples/{}/fluxes'.format(self.sample.id),
             '/samples/{}/phases'.format(self.sample.id),
+            '/samples/{}/model-options'.format(self.sample.id),
         ]
         for url in URLS:
             r = requests.get(self.api + url)
