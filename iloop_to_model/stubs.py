4393c4196367189189a240cd8b6a841268905905
Ali Kaafarani <ali@kvikshaug.no>
2018-03-12 17:28:33 +0100

    style: newline after license

--- iloop_to_model/stubs.py
+++ iloop_to_model/stubs.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 from venom.fields import MapField, Int, String, repeated, map_, Float32, Bool
 from venom.message import Message
 from venom.common.messages import JSONValue
--- tests/test_integration.py
+++ tests/test_integration.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 import pytest
 
 from iloop_to_model.iloop_to_model import fluxes, make_request, model_json, tmy
