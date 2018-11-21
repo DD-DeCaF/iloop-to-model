667dfc5ea486c6fd0ec8f9fcd0225a770dd47d50
Ali Kaafarani <ali@kvikshaug.no>
2018-11-21 16:04:13 +0100

    chore: remove staging deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -18,8 +18,6 @@ set -xeu
 
 if [ "${TRAVIS_BRANCH}" = "master" ]; then
   DEPLOYMENT=iloop-to-model-production
-elif [ "${TRAVIS_BRANCH}" = "devel" ]; then
-  DEPLOYMENT=iloop-to-model-staging
 else
   echo "Skipping deployment for branch ${TRAVIS_BRANCH}"
   exit 0
